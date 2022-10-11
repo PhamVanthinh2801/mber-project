7940d765499340bf234eff51f10d0e75a40c99ce
TSOLUTION-Pham Thinh <thinhphamndtdtu@gmail.com>
2022-10-11 14:49:37 +0700

    update

--- src/app/modules/thu-den-module/components/nhap-moi/nhap-moi.service.ts
+++ src/app/modules/thu-den-module/components/nhap-moi/nhap-moi.service.ts
@@ -1,6 +1,7 @@
 
 import {apiServiceBase} from "../../../base-module/service-base/api-service-base";
 import * as API  from "../../../base-module/service-base/api-gateway";
+
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
 
---
+++ src/app/modules/thu-den-module/components/thu-moi/nhan-vien-thu-moi/nhan-vien-thu-moi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nhan-vien-thu-moi',
+  templateUrl: './nhan-vien-thu-moi.component.html',
+  styleUrls: ['./nhan-vien-thu-moi.component.scss']
+})
+export class NhanVienThuMoiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
